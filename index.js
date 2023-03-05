915ce1c8919314b0769911e9edc2e64d5e5b7e3c
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-05 11:52:46 -0600

    crear servidor express y metodo get basico

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hola desde node');
+const express = require("express");
+
+// Crear el servidor/app de express
+const app = express();
+
+// GET
+
+app.get('/', (req, resp) => {
+    // resp.status(500) => cambiar status de la peticion
+    resp.status(500).json({
+        ok: true,
+        msg: 'Todo salio bien',
+        uid: 123
+    });
+})
+
+app.listen(4000, () => {
+    console.log(`Servidor corriendo en puerto ${4000}`);
+
+});
