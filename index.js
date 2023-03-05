f1fc9f3a5fb2598add56ee7f9c37359d5da21b8a
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-05 14:23:30 -0600

    agregar cors

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors')
 
 // Crear el servidor/app de express
 const app = express();
 
+
+// BEGIN: Middleware
+
+// Cors
+app.use(cors());
+
 // Rutas
 app.use("/api/auth", require('./routes/auth.routes'));
+
+// END: Middleware
 
 app.listen(4000, () => {
   console.log(`Servidor corriendo en puerto ${4000}`);
