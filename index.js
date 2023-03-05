137109761e9ea3cfa59ea265203ef70f78985438
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-05 12:05:30 -0600

    agregar rutas y exportarlas

--- index.js
+++ index.js
@@ -3,18 +3,9 @@ const express = require("express");
 // Crear el servidor/app de express
 const app = express();
 
-// GET
-
-app.get('/', (req, resp) => {
-    // resp.status(500) => cambiar status de la peticion
-    resp.status(500).json({
-        ok: true,
-        msg: 'Todo salio bien',
-        uid: 123
-    });
-})
+// Rutas
+app.use("/api/auth", require('./routes/auth'));
 
 app.listen(4000, () => {
-    console.log(`Servidor corriendo en puerto ${4000}`);
-
+  console.log(`Servidor corriendo en puerto ${4000}`);
 });
