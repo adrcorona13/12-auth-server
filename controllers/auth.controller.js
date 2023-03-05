eefd3a968488763bd58f755fd8405badc65b9937
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-05 14:30:14 -0600

    leer parametros del body

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,10 @@
 
 const crearUsuario = (req, res) => {
+
+    const { name, email, password } = req.body;
+
+    console.log( name, email, password );
+
     return res.json({
         ok: true,
         msg: 'Crear usuario /new'
@@ -7,6 +12,11 @@ const crearUsuario = (req, res) => {
 }
 
 const loginUsuario = (req, res) => {
+
+    const { email, password} = req.body;
+
+    console.log(email, password);
+
     return res.json({
         ok: true,
         msg: 'Login de usuario /'
