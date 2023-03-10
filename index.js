3b482dcc0d7f5087266dc2f54bbd01c488acbf3a
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-09 23:51:30 -0600

    agregar comodin ruta

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const { dbConnection } = require("./db/config");
 require('dotenv').config();
 
@@ -23,6 +24,12 @@ app.use(express.json());
 
 // Rutas
 app.use("/api/auth", require("./routes/auth.routes"));
+
+// Manejar demás rutas
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'))
+});
 
 // END: Middleware
 
